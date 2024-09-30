6d7e21e7d8a29862d1e806dca9a97631e948b729
Neopatron Latam <hola@neopatron.com>
2024-09-30 02:15:27 -0500

    url server

--- config/server.js
+++ config/server.js
@@ -1,10 +1,23 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://tu-dominio.com'), // Cambia 'https://tu-dominio.com' por tu URL de producción
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  proxy: true,  // Si estás utilizando un proxy inverso como NGINX o Apache
 });
