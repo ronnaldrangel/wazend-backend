1c669b6778b9f1ba51ab5e232d23a77ac85f4410
Ronald <ronald@rangel.pro>
2024-10-06 22:51:27 +0000

    easypanel

--- config/plugins.js
+++ config/plugins.js
@@ -1,34 +1,22 @@
 module.exports = ({ env }) => ({
-  // ...
-  email: {
-    config: {
-      provider: 'nodemailer',
-      providerOptions: {
-        host: env('SMTP_HOST', 'smtp.example.com'),
-        port: env('SMTP_PORT', 587),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
+    // ...
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.example.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: env('SMTP_USERNAME'),
+            pass: env('SMTP_PASSWORD'),
+          },
+          // ... any custom nodemailer options
         },
-        // ... any custom nodemailer options
-      },
-      settings: {
-        defaultFrom: 'Wazend <soporte@wazend.net>',
-        defaultReplyTo: 'soporte@wazend.net',
-      },
-    },
-  },
-  // ...
-  // Otras configuraciones de plugins
-  "users-permissions": {
-    config: {
-      register: {
-        allowedFields: [
-          "name",   // Permitir el campo 'name'
-          "phone"   // Permitir el campo 'phone'
-        ],
+        settings: {
+          defaultFrom: 'Wazend <soporte@wazend.net>',
+          defaultReplyTo: 'soporte@wazend.net',
+        },
       },
     },
-  },
-
-});
+    // ...
+  });
