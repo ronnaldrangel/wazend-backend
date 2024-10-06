27315bf9651690ba567ffdd08433b7730246c58f
Neopatron Latam <hola@neopatron.com>
2024-10-06 17:53:29 -0500

    register

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,34 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.example.com'),
-          port: env('SMTP_PORT', 587),
-          auth: {
-            user: env('SMTP_USERNAME'),
-            pass: env('SMTP_PASSWORD'),
-          },
-          // ... any custom nodemailer options
+  // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
         },
-        settings: {
-          defaultFrom: 'Wazend <soporte@wazend.net>',
-          defaultReplyTo: 'soporte@wazend.net',
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'Wazend <soporte@wazend.net>',
+        defaultReplyTo: 'soporte@wazend.net',
       },
     },
-    // ...
-  });
+  },
+  // ...
+  // Otras configuraciones de plugins
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: [
+          "name",   // Permitir el campo 'name'
+          "phone"   // Permitir el campo 'phone'
+        ],
+      },
+    },
+  },
+
+});
